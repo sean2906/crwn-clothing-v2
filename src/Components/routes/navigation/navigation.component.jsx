976283ce88a9d7fe1428986e696e80d742edc2c7
Sean Rodgers <sean@opengrid.com>
2024-09-12 11:06:25 +0100

    saved for section 9

--- src/Components/routes/navigation/navigation.component.jsx
+++ src/Components/routes/navigation/navigation.component.jsx
@@ -1,10 +1,14 @@
 import { Outlet, Link } from "react-router-dom";
-import { Fragment } from "react";
+import { Fragment, useContext } from "react";
 import { ReactComponent as CrwnLogo } from '../../../assests/crown.svg';
 import './navigation.styles.scss';
+import { UserContext } from "../../../contexts/user.context";
+import {signOutUser} from '../../../utils/firebase/firebase.utils.jsx'
 
 const Navigation = () => {
-    return (
+    const { currentUser } = useContext(UserContext);
+
+        return (
         <Fragment>
             <div className="navigation">
                 <Link className="logo-container" to='/'>
@@ -15,9 +19,13 @@ const Navigation = () => {
                     <Link className='nav-link' to='/shop'>
                         SHOP
                     </Link>
-                    <Link className='nav-link' to='/auth'>
+                    {currentUser ? (
+                        <span className="nav-link" onClick={signOutUser}>
+                            SIGN OUT</span>
+                    ) : (<Link className='nav-link' to='/auth'>
                         SIGN IN
                     </Link>
+                    )}
                 </div>
             </div>
             <Outlet />
--- src/utils/firebase/firebase.utils.jsx
+++ src/utils/firebase/firebase.utils.jsx
@@ -4,7 +4,9 @@ import {
     signInWithRedirect, 
     signInWithPopup, GoogleAuthProvider, 
     createUserWithEmailAndPassword, 
-    signInWithEmailAndPassword 
+    signInWithEmailAndPassword,
+    signOut,
+    onAuthStateChanged,
 } from 'firebase/auth';
 
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
@@ -74,5 +76,11 @@ export const createAuthUserWithEmailAndPassword = async (email, password) => {
 export const signInAuthUserWithEmailAndPassword = async (email, password) => {
     if (!email || !password) return;
 
-    return await signInAuthUserWithEmailAndPassword(auth, email, password)
+    return await signInWithEmailAndPassword(auth, email, password)
 };
+
+export const signOutUser = async () => await signOut(auth);
+
+export const onAuthStateChangedListener = (callback) => {
+    onAuthStateChanged(auth, callback);
+}
